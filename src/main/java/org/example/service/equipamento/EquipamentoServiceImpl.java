6cad6e664c1925a4f46eca5062ac6b344b0309ab
eduardo_d_maia <eduardo_d_maia@estudante.sesisenai.org.br>
2025-11-25 21:54:05 -0300

    primeiro commit

--- src/main/java/org/example/service/equipamento/EquipamentoServiceImpl.java
+++ src/main/java/org/example/service/equipamento/EquipamentoServiceImpl.java
@@ -1,17 +1,39 @@
-package org.example.service.equipamento;
+package org.example.service.acaocorretiva;
 
-import org.example.model.Equipamento;
+import org.example.model.AcaoCorretiva;
+import org.example.model.Falha;
+import org.example.repositorio.AcaoCorretivaRepositorio;
+import org.example.repositorio.EquipamentoRepositorio;
+import org.example.repositorio.FalhaRepositorio;
 
 import java.sql.SQLException;
 
-public class EquipamentoServiceImpl implements EquipamentoService{
-    @Override
-    public Equipamento criarEquipamento(Equipamento equipamento) throws SQLException {
-        return null;
-    }
+public class AcaoCorretivaServiceImpl implements AcaoCorretivaService{
+    FalhaRepositorio falhaRepositorio = new FalhaRepositorio();
+
+    EquipamentoRepositorio equipamentoRepositorio = new EquipamentoRepositorio();
+
+    AcaoCorretivaRepositorio acaoRepositorio = new AcaoCorretivaRepositorio();
 
     @Override
-    public Equipamento buscarEquipamentoPorId(Long id) throws SQLException {
-        return null;
+    public AcaoCorretiva registrarConclusaoDeAcao(AcaoCorretiva acao) throws SQLException {
+        Falha falha = falhaRepositorio.buscarFalhaPorId(acao.getFalhaId());
+
+        if(falha == null){
+            throw new RuntimeException("Falha não encontrada!");
+        }
+
+        acao = acaoRepositorio.criarAcaoCorretiva(acao);
+
+        falhaRepositorio.atualizarStatus(acao.getFalhaId(), "RESOLVIDA");
+
+        if(falha.getCriticidade().equals("CRITICA")){
+            equipamentoRepositorio.atualizarStatus(falha.getEquipamentoId(), "OPERACIONAL");
+        }
+
+        if(acao.getId() == null){
+            throw new RuntimeException("Erro ao salvar Ação no banco de dados!");
+        }
+        return acao;
     }
 }
